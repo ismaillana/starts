62f6c9ec7ddc2c6dfa22a21cd541bc2083056143
Lana <ismaillana22@gmail.com>
2023-12-08 16:47:44 +0700

    layout base 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/users/base');
+});
+
+Route::get('/dashboard-admin', function () {
+    return view('layouts/admin/base');
 });
